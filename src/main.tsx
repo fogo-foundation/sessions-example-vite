8bf528a2170067035817cc2e338634e8a5a389ef
Connor Prussin <connor@prussin.net>
2025-07-29 18:30:34 -0700

    Update sdk packages

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { FogoSessionProvider, SessionButton } from "@fogo/sessions-sdk-react";
 import { NATIVE_MINT } from "@solana/spl-token";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { loadSponsorKey } from "./sponsor-key";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FogoSessionProvider
       endpoint="https://testnet.fogo.io/"
-      sponsor={loadSponsorKey(import.meta.env.VITE_SPONSOR_KEY).publicKey.toBase58()}
-      paymasterUrl="/paymaster"
       tokens={[NATIVE_MINT.toBase58()]}
       defaultRequestedLimits={{
         [NATIVE_MINT.toBase58()]: 1_500_000_000n,
--- src/sponsor-key.ts
+++
@@ -1,5 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-
-export const loadSponsorKey = (value: string) => Keypair.fromSecretKey(
-  Uint8Array.from(JSON.parse(value) as number[])
-);
